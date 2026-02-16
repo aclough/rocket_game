c6398d14632f535deece0fc220f360d626906aca
Andrew Clough <andrew.clough@gmail.com>
2026-02-16 11:22:34 -0500

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 sudo apt install flatpak
-flatpak install flathub org.godotengine.Godot
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.godotengine.Godot
